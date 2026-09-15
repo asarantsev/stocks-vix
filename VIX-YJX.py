3c545f7065303c2282b1276624c284df4ea9f485
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-09-14 21:01:00 -0700

    Add files via upload

--- VIX-YJX.py
+++ VIX-YJX.py
@@ -5,10 +5,22 @@ from statsmodels.tsa.stattools import acf
 
 DF = pd.read_excel('VIX.xlsx', sheet_name = 'data')
 vix = DF['VIX'].values
+N = len(vix)
+
 lvix = np.log(vix)
 lam = stats.yeojohnson(lvix)[1]
+print('lambda = ', lam)
 tvix = stats.yeojohnson(lvix)[0]
+
+print('Shapiro-Wilk p = ', stats.shapiro(tvix)[1])
+print('Jarque-Bera p = ', stats.jarque_bera(tvix)[1])
+
 Reg = stats.linregress(tvix[:-1], tvix[1:])
-resid = tvix[1:] - Reg.slope * tvix[:-1] - Reg.intercept * np.ones(479)
-print(acf(resid, nlags = 10, qstat = True))
-print(acf(abs(resid), nlags = 10, qstat = True))
+resid = tvix[1:] - Reg.slope * tvix[:-1] - Reg.intercept * np.ones(N - 1)
+print(Reg)
+print('ACF for original residuals')
+print(acf(resid, nlags = 10, qstat = True)[2])
+print('ACF for absolute values')
+print(acf(abs(resid), nlags = 10, qstat = True)[2])
+print('Shapiro-Wilk p = ', stats.shapiro(resid)[1])
+print('Jarque-Bera p = ', stats.jarque_bera(resid)[1]) 
